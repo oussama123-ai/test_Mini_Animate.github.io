1331abed263d3ef1fda800874d3c5a2bc636e35f
Oussama Othmani <83811606+oussama123-ai@users.noreply.github.com>
2022-10-03 17:45:59 +0200

    Update demo4.js

--- Mini-Projet/js/demo4.js
+++ Mini-Projet/js/demo4.js
@@ -1,13 +1,4 @@
-/**
- * demo4.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2019, Codrops
- * http://www.codrops.com
- */
+
 {
     const MathUtils = {
         lineEq: (y2, y1, x2, x1, currentVal) => {
